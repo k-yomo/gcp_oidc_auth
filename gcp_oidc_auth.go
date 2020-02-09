3ddb87bb507c7d688b1d3569d45dd937c7339a9e
k-yomo <kanji.yy@gmail.com>
2020-02-09 17:03:10 +0900

    Implement gcp oidc id token auth library

---
+++ gcp_oidc_auth.go
@@ -0,0 +1,59 @@
+package gcp_oidc_auth
+
+import (
+	"context"
+	"github.com/coreos/go-oidc"
+	"github.com/pkg/errors"
+)
+
+const (
+	googleRootCertURL = "https://www.googleapis.com/oauth2/v3/certs"
+	oidcTokenIssueURL = "https://accounts.google.com"
+)
+
+// verifier is oidc.IDTokenVerifier
+// to cache remote key set, verifier is hold statically in-memory
+var verifier *oidc.IDTokenVerifier
+
+// Config is the configuration for VerifyIDToken.
+type Config struct {
+	// Expected audience of the token.
+	// If not provided, skipAudienceCheck must be set explicitly
+	aud string
+	// If true, no audience check performed. Must be true if aud field is empty.
+	skipAudienceCheck bool
+}
+
+func VerifyIDToken(ctx context.Context, serviceAccountEmail, oidcToken string, config *Config) error {
+	idToken, err := verifyGoogleIDToken(ctx, oidcToken, config)
+	if err != nil {
+		return err
+	}
+	var idTokenClaims struct {
+		Email         string `json:"email"`
+		EmailVerified bool   `json:"email_verified"`
+	}
+	if err := idToken.Claims(&idTokenClaims); err != nil {
+		return errors.Wrap(err, "decode idToken claims")
+	}
+	if !idTokenClaims.EmailVerified {
+		return errors.Errorf("issuer email %s is not verified", idTokenClaims.Email)
+	}
+	if idTokenClaims.Email != serviceAccountEmail {
+		return errors.Errorf("unexpected issuer email '%s'", idTokenClaims.Email)
+	}
+	return nil
+}
+
+func verifyGoogleIDToken(ctx context.Context, token string, config *Config) (*oidc.IDToken, error) {
+	if verifier == nil {
+		keySet := oidc.NewRemoteKeySet(ctx, googleRootCertURL)
+		config := &oidc.Config{ClientID: config.aud, SkipClientIDCheck: config.skipAudienceCheck}
+		verifier = oidc.NewVerifier(oidcTokenIssueURL, keySet, config)
+	}
+	idToken, err := verifier.Verify(ctx, token)
+	if err != nil {
+		return nil, errors.Wrap(err, "verify idToken")
+	}
+	return idToken, nil
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/k-yomo/gcp_oidc_auth
+
+go 1.13
+
+require (
+	github.com/coreos/go-oidc v2.2.1+incompatible // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
+	golang.org/x/crypto v0.0.0-20200208060501-ecb85df21340 // indirect
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/square/go-jose.v2 v2.4.1 // indirect
+)
