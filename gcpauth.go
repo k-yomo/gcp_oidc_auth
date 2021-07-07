9db451f55822af069a4a8d1df5fbd4eba01d36de
k-yomo <kanji.yy@gmail.com>
2021-07-08 03:14:53 +0900

    Fix errors

--- gcpauth.go
+++ gcpauth.go
@@ -15,19 +15,25 @@ const (
 // to cache remote key set, verifier is hold statically in-memory
 var verifier *oidc.IDTokenVerifier
 
+var (
+	IDTokenNotVerified = errors.New("issuer email is not verified")
+	IssuerEmailNotVerified = errors.New("issuer email is not verified")
+	UnexpectedIssuerEmail = errors.New("unexpected issuer email")
+)
+
 // Config is the configuration for VerifyIDToken.
 type Config struct {
 	// Expected audience of the token.
-	// If not provided, skipAudienceCheck must be set explicitly
-	aud string
-	// If true, no audience check performed. Must be true if aud field is empty.
-	skipAudienceCheck bool
+	// If not provided, SkipAudienceCheck must be set explicitly
+	Aud string
+	// If true, no audience check performed. Must be true if Aud field is empty.
+	SkipAudienceCheck bool
 }
 
 func VerifyIDToken(ctx context.Context, serviceAccountEmail, oidcToken string, config *Config) error {
 	idToken, err := verifyGoogleIDToken(ctx, oidcToken, config)
 	if err != nil {
-		return err
+		return IDTokenNotVerified
 	}
 	var idTokenClaims struct {
 		Email         string `json:"email"`
@@ -37,10 +43,10 @@ func VerifyIDToken(ctx context.Context, serviceAccountEmail, oidcToken string, c
 		return errors.Wrap(err, "decode idToken claims")
 	}
 	if !idTokenClaims.EmailVerified {
-		return errors.Errorf("issuer email %s is not verified", idTokenClaims.Email)
+		return IssuerEmailNotVerified
 	}
 	if idTokenClaims.Email != serviceAccountEmail {
-		return errors.Errorf("unexpected issuer email '%s'", idTokenClaims.Email)
+		return UnexpectedIssuerEmail
 	}
 	return nil
 }
@@ -48,12 +54,12 @@ func VerifyIDToken(ctx context.Context, serviceAccountEmail, oidcToken string, c
 func verifyGoogleIDToken(ctx context.Context, token string, config *Config) (*oidc.IDToken, error) {
 	if verifier == nil {
 		keySet := oidc.NewRemoteKeySet(ctx, googleRootCertURL)
-		config := &oidc.Config{ClientID: config.aud, SkipClientIDCheck: config.skipAudienceCheck}
+		config := &oidc.Config{ClientID: config.Aud, SkipClientIDCheck: config.SkipAudienceCheck}
 		verifier = oidc.NewVerifier(oidcTokenIssueURL, keySet, config)
 	}
 	idToken, err := verifier.Verify(ctx, token)
 	if err != nil {
-		return nil, errors.Wrap(err, "verify idToken")
+		return nil, err
 	}
 	return idToken, nil
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/k-yomo/gcp_oidc_auth
 go 1.13
 
 require (
-	github.com/coreos/go-oidc v2.2.1+incompatible // indirect
+	github.com/coreos/go-oidc v2.2.1+incompatible
 	github.com/golang/protobuf v1.3.3 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20200208060501-ecb85df21340 // indirect
 	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
